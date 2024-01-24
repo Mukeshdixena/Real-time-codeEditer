b1640d270b7efd48eb06807570ca053fbff05169
shah6386 <mohdshahzad638683@gmail.com>
2024-01-24 15:49:11 +0530

    creadted home page and added some styles

--- code_editor/src/pages/Home.js
+++ code_editor/src/pages/Home.js
@@ -5,32 +5,37 @@ const Home = () => {
     <div className='homePageWrapper'>
 
       <div className='formWrapper'>
-        <img src="/img/Code-sync-logo.jpeg" alt="code-sync-logo"></img>
+        <img src="/img/codedit.png" alt="code-sync-logo"></img>
 
         <h4 className='mainLabel'> Paste invitation ROOM ID</h4>
 
+
+
         <div className='inputeGroup'>
           <input type="text" className='inputBox' placeholder='ROOM ID' />
-
-        </div>
-
-        <div className='inputeGroup'>
           <input type="text" className='inputBox' placeholder='USERNAME' />
 
-        </div>
 
-        <button className='btn joinBtn'>Join</button>
+          <button className='btn joinBtn'>Join</button>
+        </div>
 
         <span className='createInfo'>
 
           If you don't have an invite then create &nbsp;
-          
+
           <a href='#' className='createNewBtn'>
             new room
           </a>
         </span>
       </div>
 
+
+      <footer>
+        <h4>
+          Build with &#9824; by <a href="https://github.com/shah6386">Shahzad</a> & &nbsp;
+          <a href="https://github.com/Mukeshdixena">Mukesh</a>
+        </h4>
+      </footer>
     </div>
   )
 }
