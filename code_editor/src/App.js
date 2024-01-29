7c007a99af496a2ad8bc1718714708bdfd2ce50f
shah6386 <mohdshahzad638683@gmail.com>
2024-01-29 22:56:18 +0530

    deleted unneccesary files

--- code_editor/src/App.js
+++ code_editor/src/App.js
@@ -23,7 +23,7 @@ function App() {
 
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}><Route />
+          <Route path="/" element={<Home/>}><Route />
           </Route>
 
           <Route path="/editor/:roomId" element={<EditorPage />}><Route />
