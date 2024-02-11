a35057ec44c314c69109ece6130531ffe6eb8470
Mukesh <mukeshdixena123@gmail.com>
2024-02-11 22:57:03 +0530

    connected websocket and server

---
+++ code_editor/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require('socket.io');
+const server = http.createServer(app);
+const ACTIONS = require('./src/Actions');
+
+const io = new Server(server);
+
+const userSocketMap = {};
+
+function getAllConnectedClients(roomId) {
+    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+        (socketId) => {
+            return {
+                socketId,
+                userName: userSocketMap[socketId],
+
+            };
+        });
+
+}
+
+io.on('connection', (socket) => {
+    console.log('socket connected', socket.id);
+
+    socket.on(ACTIONS.JOIN, ({ roomId, userName }) => {
+        userSocketMap[socket.id] = userName;
+        socket.join(roomId);
+        const clients = getAllConnectedClients(roomId);
+        console.log(clients);
+    });
+});
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- code_editor/src/pages/EditorPage.js
+++ code_editor/src/pages/EditorPage.js
@@ -1,14 +1,50 @@
-import React, { useState } from 'react'
-import Client from '../components/Client.js'
-import Editor from '../components/EditorArea.js'
+import React, { useState, useRef, useEffect } from 'react';
+import toast from 'react-hot-toast';
+import Client from '../components/Client.js';
+import Editor from '../components/EditorArea.js';
+import { initSocket } from '../socket.js';
+import ACTIONS from '../Actions.js';
+import { useLocation, useNavigate, Navigate, useParams } from 'react-router-dom';
 
 const EditorPage = () => {
+
+  const socketRef = useRef(null);
+  const location = useLocation();
+  const { roomId } = useParams();
+  const reactNavigator = useNavigate();
+  useEffect(() => {
+    const init = async () => {
+      socketRef.current = await initSocket();
+
+
+      socketRef.current.on('connect_error', (err) => handleErrors(err));
+      socketRef.current.on('connect_failed', (err) => handleErrors(err));
+
+      function handleErrors(e) {
+        console.log('socket error', e);
+
+        toast.error('socket connection failed, try agian later.');
+        reactNavigator('/');
+      }
+
+      socketRef.current.emit(ACTIONS.JOIN, {
+        roomId,
+        userName: location.state?.userName,
+      });
+    }
+    init();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const [clients, setClients] = useState([
     { socketId: 1, userName: 'Mukesh' },
     { socketId: 2, userName: 'Sha MD' },
-    { socketId: 3, userName: 'Sha MD' },
+
   ]);
+  if (!location.state) {
+
+    return <Navigate to="/" />;
+  }
   return (
     <div className='mainWrap'>
       <div className='aside'>
@@ -35,7 +71,7 @@ const EditorPage = () => {
 
       </div>
       <div className='aditorWrop'>
-        <Editor/>
+        <Editor />
       </div>
     </div>
   )
